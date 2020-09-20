fc74bbee6c19abb4f8a9d3242e8c8a9f8682cd40
Adebiyi Daniel <danifara.3.ad@gmail.com>
2020-09-20 15:15:41 +0100

    fixed unit testing

--- src/index.test.js
+++ src/index.test.js
@@ -1,7 +1,7 @@
 //for the assertion in unit testing
 import {expect} from 'chai';
-// import { jsdom } from 'jsdom';
-// import {fs} from 'fs';
+import jsdom from 'jsdom';
+import fs from 'fs';
 
 
 //sample unit test code
@@ -12,26 +12,26 @@ describe('Our first test', ()=>{
   });
 });
 
-// describe('index.html', () => {
-//   it('should say hello',(done) => {
-//     const index = fs.readFileSync('./src/index.html', "utf-8");
-//     jsdom.env(index, function(err, window){
-//       const h1 = window.document.getElementsByTagName('h1')[0];
-//       expect(h1.innerHTML).to.equal("Hello World");
-//       done();
-//       window.close();
-//     });
-//   });
-// });
+describe('index.html', () => {
+  it('should say hello',(done) => {
+    const index = fs.readFileSync('./src/index.html', "utf-8");
+    jsdom.env(index, function(err, window){
+      const h2 = window.document.getElementsByTagName('h2')[0];
+      expect(h2.innerHTML).to.equal("Hello World");
+      done();
+      window.close();
+    });
+  });
+});
 
-// describe('index.html', () => {
-//   it('should have h1 that says users',(done) => {
-//     const index = fs.readFileSync('./src/index.html', "utf-8");
-//     jsdom.env(index, function(err, window){
-//       const h1 = window.document.getElementsByTagName('h1')[0];
-//       expect(h1.innerHTML).to.equal("Users");
-//       done();
-//       window.close();
-//     });
-//   });
-// });
+describe('index.html', () => {
+  it('should have h1 that says users',(done) => {
+    const index = fs.readFileSync('./src/index.html', "utf-8");
+    jsdom.env(index, function(err, window){
+      const h1 = window.document.getElementsByTagName('h1')[0];
+      expect(h1.innerHTML).to.equal("Users");
+      done();
+      window.close();
+    });
+  });
+});
